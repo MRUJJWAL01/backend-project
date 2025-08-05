6fd1f2c55afd8853388ca174cc19286fbd82f07c
Ujjwal Singh Chouhan <your-email@example.com>
2025-08-06 01:43:55 +0530

    refactor song upload route to enhance logging and clean up commented code

--- backend/src/routes/song.routes.js
+++ backend/src/routes/song.routes.js
@@ -1,44 +1,38 @@
 const express = require("express");
-
 const songModel = require("../models/song.model");
 const router = express.Router();
 const multer = require("multer");
 const id3 = require("node-id3");
 const app = require("../app");
-
 const uploadFile = require("../services/storage.service");
 const upload = multer({ storage: multer.memoryStorage() });
-
 router.post("/songs", upload.single("audio"), async (req, res) => {
   try {
     const buffer = req.file.buffer;
-    // console.log(buffer);
-
     const base64File = Buffer.from(buffer).toString("base64");
-    // console.log(base64File);
-
     const response = id3.read(buffer);
-    //  console.log(response);
-
     const result = await uploadFile(base64File, "hello");
-    // console.log(result);
-
-    // const coverImageResult = await uploadFile(
-    //   Buffer.from(response.image.imageBuffer).toString("base64"),
-    //   "coverImage"
-    // );
-
+    console.log(result);
+    let coverImageResult = null;
+    if (response?.image?.imageBuffer) {
+     coverImageResult = await uploadFile(
+        Buffer.from(response.image.imageBuffer).toString("base64"),
+        "coverImage"
+      );
+    }
+    console.log(coverImageResult);
+    
     const song = await songModel.create({
       title: response.title,
       artist: response.artist,
       album: response.album,
       releaseDate: response.year,
       audioUrl: result.url,
-      //coverImage: coverImageResult.url,
+      coverImage: coverImageResult?.url || null,
     });
-
     res.status(201).json({
       message: "success",
+      song
     });
   } catch (error) {
     console.log(error);
